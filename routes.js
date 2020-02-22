60d321b578ecf8dde654a7c1eb0d6022d9ef924e
abraao999 <45611524+abraao999@users.noreply.github.com>
2020-02-22 10:42:57 -0300

    comentario em routes

--- routes.js
+++ routes.js
@@ -8,25 +8,31 @@ const presenca = require("./src/controllers/presenca");
 const classe = require("./src/controllers/classe");
 const routes = express.Router();
 
+//aluno
 routes.get("/lista/:congregacao/:classe", alunos.alunosCongregacao);
 routes.get("/listaAlunos", alunos.listaAlunos);
 routes.post("/addAluno", alunos.cadastraAluno);
 
+//revista
 routes.get("/revista/:trimestre/:ano", revista.listaRevista);
 routes.post("/addRevista", revista.addRevista);
 
+//usuario
 routes.post("/addUsuario", usuario.addUsuario);
 routes.post("/user", usuario.buscaUsuario);
 
+//oferta
 routes.post("/addOferta", oferta.addOferta);
 routes.put("/revistaPago/:id", oferta.revistaPago);
 
+//presenca
 routes.post("/addPresenca", presenca.addPresenca);
 routes.delete(
   "/deletaPresenca/:trimestre/:codAluno/:ano/:licao",
   presenca.deletaPresenca
 );
 
+//classe
 routes.post("/addClasse", classe.addClasse);
 routes.get("/listaClasse/:codCongregacao", classe.listaClasse);
 routes.delete("/deletaClasse/:id", classe.deletaClasse);
